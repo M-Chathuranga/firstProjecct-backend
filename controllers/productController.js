a60086481f4599917d9012e1b601aab712653c8e
M-Chathuranga <matheeshachaturanga@gmail.com>
2025-12-14 21:48:11 +0530

    1.2.1 ✏️

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,138 +1,127 @@
 import Product from "../models/product.js";
 import { isAdmin } from "./userController.js";
 
+export async function createProduct(req, res) {
+  if (!isAdmin(req)) {
+    return res.status(403).jsonson({
+      message: "Access denied. Admins only",
+    });
+  }
 
+  const product = new Product(req.body);
 
+  try {
+    const response = await product.save();
 
-export async function createProduct(req,res) {
-    if( ! isAdmin(req)) {
-        return res.status(403).jsonson(
-            {
-                message : "Access denied. Admins only"
-            }
-        )
-    }
-
-    const product = new Product(req.body);
-
-    try{
-        const response = await product.save();
-
-        res.json({
-            message : "Product created successfuly",
-            product : response
-
-        });
-
-    }catch (error) {
-        console.log("Error creating product",error);
-        return res.status(500).json({
-            message : "faild to create product"
-        })
-    }
-   
-    
+    res.json({
+      message: "Product created successfuly",
+      product: response,
+    });
+  } catch (error) {
+    console.log("Error creating product", error);
+    return res.status(500).json({
+      message: "faild to create product",
+    });
+  }
 }
 
-
-export async function getProducts(req,res) {
-    try{
-        if(isAdmin(req)){
-            const products = await Product.find();
-            return res.json(products);
-
-        }else{
-            const products = await Product.find({isAvailable : true});
-            return res.json(products);
-        }
-    }catch (error) {
-        console.error("Error fetching products:", error);
-        return res.status(500).json({ message : "Faild to fetch products"});
+export async function getProducts(req, res) {
+  try {
+    if (isAdmin(req)) {
+      const products = await Product.find();
+      return res.json(products);
+    } else {
+      const products = await Product.find({ isAvailable: true });
+      return res.json(products);
     }
-       
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    return res.status(500).json({ message: "Faild to fetch products" });
+  }
 }
 
+export async function deleteProduct(req, res) {
+  if (!isAdmin(req)) {
+    res.status(403).json({
+      message: "Access denied. Addmins only",
+    });
+    return;
+  }
 
-export async function deleteProduct(req,res) {
-    if( ! isAdmin(req)){
-        res.status(403).json({
-            message : "Access denied. Addmins only"
-        });
-        return;
-
-    }
-
-    try{
-        const productId = req.params.productId;
-
-        await Product.deleteOne({
-            productId : productId
-        });
-
-        res.json({ message : "Product deletedd successfully"});
-    }catch (error) {
-        console.log("Error deleting product", error);
-        res.status(500).json({ message : "Faild To delete product"});
-        return;
-    }
-}
-
-
-export async function updateProduct(req,res) {
-    if( ! isAdmin(req)){
-        res.status(403).json({ message : "Access denied. Admins only"});
-        return;
-    }
-
-    const data = req.body;
+  try {
     const productId = req.params.productId;
-    // to prevent overwriting the productId in the request body
-    data.productId = productId;
 
-    try{
-        await Product.updateOne(
-            {
-                productId : productId
-            },
-            data
-        );
-        res.json({ message : "Product updated successfully"});
-    }catch (error) {
-        console.log("Error updating product",error);
-        res.status(500).json({message : " Faild to update product"});
-        return;
+    const product = await Product.findOne({ productId: productId });
+
+    if (!product) {
+      res.status(401).json({
+        message: "Product no found",
+      });
+      return;
     }
+
+    await Product.deleteOne({
+      productId: productId,
+    });
+
+    res.json({ message: "Product deletedd successfully" });
+  } catch (error) {
+    console.log("Error deleting product", error);
+    res.status(500).json({ message: "Faild To delete product" });
+    return;
+  }
 }
 
-export async function getProductInfo(req,res) {
-    try{
-        const productId =req.params.productId;
-        const product = await Product.findOne({productId : productId});
+export async function updateProduct(req, res) {
+  if (!isAdmin(req)) {
+    res.status(403).json({ message: "Access denied. Admins only" });
+    return;
+  }
 
-        if(product == null){
-            res.status(404).json({ message : "Product not found"});
-            return;
-        }
+  const data = req.body;
+  const productId = req.params.productId;
+  // to prevent overwriting the productId in the request body
+  data.productId = productId;
 
-        if( ! isAdmin){
-            res.json(product);
+  try {
+    await Product.updateOne(
+      {
+        productId: productId,
+      },
+      data
+    );
+    res.json({ message: "Product updated successfully" });
+  } catch (error) {
+    console.log("Error updating product", error);
+    res.status(500).json({ message: " Faild to update product" });
+    return;
+  }
+}
 
-        }else{
-            if(product.isAvailable){
-                res.json(product);
-            }else{
-                res.status(404).json({ message : "Product is not available"});
-            }
+export async function getProductInfo(req, res) {
+  try {
+    const productId = req.params.productId;
+    const product = await Product.findOne({ productId: productId });
 
-        }
-
-
-    }catch(error) {
-        console.log("Error fetching product info", error);
-        res.status(500).json({ message : "Faild to fetch product info"});
-        return;
+    if (product == null) {
+      res.status(404).json({ message: "Product not found" });
+      return;
     }
-    
+
+    if (!isAdmin) {
+      res.json(product);
+    } else {
+      if (product.isAvailable) {
+        res.json(product);
+      } else {
+        res.status(404).json({ message: "Product is not available" });
+      }
+    }
+  } catch (error) {
+    console.log("Error fetching product info", error);
+    res.status(500).json({ message: "Faild to fetch product info" });
+    return;
+  }
 }
 
 // export async function searchProducts(req,res) {
@@ -153,6 +142,5 @@ export async function getProductInfo(req,res) {
 //             message : "Faild to search products"
 //         });
 //     }
-    
-    
+
 // }
